ad5eb7cbf638aedba78751ae0b31e22fc5e0d35a
havebear <616749285@qq.com>
2020-10-28 16:59:25 +0800

    chore: 变更page目录，增加accest目录，变更首页布局

---
+++ packages/component-studio-ui/src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../accets/style/index.less';
+import styles from './index.less';
+
+export default () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}></div>
+      <div className={styles.body}></div>
+    </div>
+  );
+};
